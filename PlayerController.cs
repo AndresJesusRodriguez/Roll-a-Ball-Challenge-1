574c020c86d931fc1b78ad87aaf55c8ec2bf5faa
AndresJesusRodriguez <97657587+AndresJesusRodriguez@users.noreply.github.com>
2022-02-06 22:16:08 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+
+    public TextMeshProUGUI livesText;
+    public GameObject winTextObject;
+
+    public GameObject loseTextObject;
+    private Rigidbody rb;
+    private int count;
+
+    private int lives;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        lives = 3;
+        SetCountText();
+        SetLivesText();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "count: " + count.ToString();
+        if (count >= 20)
+        {
+            winTextObject.SetActive(true);
+            Destroy(gameObject);
+        }
+        if (count == 12)
+        {
+            transform.position = new Vector3(50.0f, 0.0f, 50.0f);
+        }
+    }
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            loseTextObject.SetActive(true);
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+
+            SetLivesText();
+        }
+
+
+    }
+
+}
